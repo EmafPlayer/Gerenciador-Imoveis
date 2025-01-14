6e469c0fa87540749712fe5d08103070700f2d9e
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-01-14 00:43:47 -0300

    Fazendo alterações no frontend

--- frontend/src/components/nav_bar.tsx
+++ frontend/src/components/nav_bar.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from './nav_link';
 import { useLocation } from 'react-router-dom';
 import FotoCasa from '../../public/Foto-Casa.png'
+import { AiOutlineHome } from "react-icons/ai";
 
 type UserProps = {
   user: {
@@ -18,10 +19,11 @@ export function NavBar( { user }: UserProps ){
 
   return(
       <nav className="h-[11%] w-full flex items-center justify-between px-12 bg-[#995935] bg-gradient-to-r from-[#995935] to-[#996B46] fixed z-10">
-          <div className='flex items-center justify-between'>
-            <img src={FotoCasa} alt="Logo" className="h-[100px] w-[100px]" />
-            <a href="/" className="text-[#FFFFFF] w-full font-sans text-[24px] justify-center uppercase">Move-IN</a>
-          </div>
+          <a href="/" className='flex items-center justify-between gap-5'>
+            {/* <img src={FotoCasa} alt="Logo" className="h-[100px] w-[100px]" /> */}
+            <div className='text-[50px] text-[#FFFFFF] '><AiOutlineHome/></div>
+            <h1 className="text-[#FFFFFF] w-full font-sans text-[24px] pt-1 justify-center uppercase">Move-IN</h1>
+          </a>
           <div className="flex items-center justify-between gap-5 pr-4">
             <h1 className='text-[#ffffff] font-sans text-[19px]'>{user.nome}</h1>
             <img src={`../../public/${user.foto}`} alt="Foto Usuário" className='h-[50px] w-[50px] rounded-full' />
