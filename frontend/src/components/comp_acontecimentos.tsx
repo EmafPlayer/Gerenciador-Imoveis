e9509c04b0aaed3cf4892cfa7d80b5b9c5f416f4
EmafPlayer <emanuelfoliveira05@gmail.com>
2025-02-05 15:20:44 -0300

    Modificando a parte de Despesas e Receitas

--- frontend/src/components/comp_acontecimentos.tsx
+++ frontend/src/components/comp_acontecimentos.tsx
@@ -27,7 +27,7 @@ export function CompAcontecimentos ( ) {
         [
             {
                 titulo: "Reunião de Planejamento",
-                descricao: "30/01/2025: Discussão sobre estratégias do próximo semestre. 05/02/2025: Realinhamento das coisas. 10/03/2025: Possibilidade de aluguel",
+                descricao: "Um dos contos presentes no livro “Laços de Família”, de Clarice Lispector, acompanha a epifania da personagem Ana ao fugir de seus afazeres domésticos. Ela, que se via sentenciada a cuidar da casa e dos filhos, assemelha-se a muitas mulheres brasileiras, que exercem essa e outras tarefas diariamente, sem valorização e, até mesmo, sem remuneração. Nesse sentido, cabe analisar as causas socioeconômicas da invisibilidade do trabalho de cuidado no Brasil contemporâneo. Em primeira perspectiva, a sociedade limita a mulher e sua função social ao ambiente caseiro e à realização de cuidados especiais. Isso ocorre porque, de acordo com o corpo social estabelecido, a essência cuidadosa é algo inerente ao feminino, muitas vezes associada à maternidade. Todavia, essa característica é construída e imposta às mulheres, que são frequentemente moldadas — assim como elucidado por Simone de Beauvoir: “Não se nasce mulher, torna-se”. Esse cenário é instigado pela cultura patriarcal e machista da nação, que atribui o cuidado e o lar somente ao sexo feminino. Desse modo, esse trabalho é visto como uma obrigação da mulher, e não como um trabalho de fato, o que, por conseguinte, gera a desvalorização de tão importante exercício. Ademais, o cuidado não é percebido com valor de mercado. Isso, porque não é uma atividade altamente lucrativa e produtiva, do ponto de vista mercadológico, o que, segundo Byung Chul-Han em “A Sociedade do cansaço”, são fatores valorizados nos dias atuais. Esse panorama se dá pela lógica capitalista que norteia as relações de trabalho no mundo hoje, priorizando o lucro de indústrias e empresas em detrimento do cuidado com pessoas — majoritariamente exercido por mulheres. Consequentemente, há a má remuneração dessa ocupação, o que afeta a igualdade de gênero na inserção no mercado de trabalho e atrapalha a emancipação feminina. Portanto, fazem-se evidentes as matrizes da invisibilidade do trabalho de cuidado em solo nacional. Logo, não se deve hesitar: são necessárias medidas para a erradicação da problemática. É responsabilidade, então, do Ministério da Educação — órgão federal que gere o ensino brasileiro — alterar a estrutura machista e patriarcal nas salas de aula. Isso pode ser feito por meio da inserção na Base Nacional Comum Curricular de formas de empoderamento feminino como assunto obrigatório na formação cidadã. Essa mudança deve ser alcançada com a finalidade de valorizar o trabalho exercido por mulheres, principalmente os mais invisíveis, como o de cuidado. Outrossim, cabe ao Governo Federal aumentar o salário mínimo atual, com o objetivo de garantir uma remuneração adequada a todos, bem como às mulheres, que se ocupam com o cuidado, favorecendo suas independências financeiras. Quem sabe, assim, todas as “Anas” que cuidam do Brasil tornar-se-ão visíveis, valorizadas e prestigiadas.",
                 data_hora_inicio: new Date("2025-01-06T09:00:00"),
                 ultima_alteracao: new Date("2025-01-30T11:30:00"),
                 status_acontecimento: 1, // Em andamento
@@ -113,7 +113,7 @@ export function CompAcontecimentos ( ) {
             <div className="w-full grid grid-cols-1 gap-10 px-14 mb-7">
 
                 {acontecimentos[contador].map((acontecimento, index) => 
-                    <div className="bg-[#f0f0f0d3] h-[270px] rounded-xl px-6 py-4 border-2 border-[#c7c7c7] shadow-md col-span-1 flex">
+                    <div className="bg-[#f0f0f0d3] h-[300px] rounded-xl px-6 py-4 border-2 border-[#c7c7c7] shadow-md col-span-1 flex">
                         <div className="pl-8 pr-14 border-r-2 border-slate-700 flex items-center">
                             <DataEstilizada data={acontecimento.data_hora_inicio}/>
                         </div>
@@ -121,7 +121,7 @@ export function CompAcontecimentos ( ) {
                             <div className="h-full flex flex-col justify-between">
                                 <div className="flex flex-col items-start">
                                     <h1 className="text-[30px] mb-3">{acontecimento.titulo}</h1>
-                                    <h2 className="text-[22px] mb-6">{acontecimento.descricao}</h2>
+                                    <div className="text-[22px] mb-6 overflow-auto overscroll-contain h-16">{acontecimento.descricao}</div>
                                     <div>
                                         <h1 className="text-[18px] font-semibold">Status do Acontecimento:</h1>
                                         <h1 className="text-[17px]">{acontecimento.status_acontecimento == 0 ? 'Planejamento' : 
@@ -131,7 +131,7 @@ export function CompAcontecimentos ( ) {
                                 <div className="flex flex-col items-end">
                                     <div className="flex flex-col items-center gap-2">
                                         <h1 className="text-[20px] font-outfit">{dayjs().to(acontecimento.ultima_alteracao)}</h1>
-                                        <h1 className="text-[12px]">Última Alteração</h1>
+                                        <h1 className="text-[14px]">Última Alteração</h1>
                                     </div>
                                 </div>
                             </div>
